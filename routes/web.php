b8c3b3e82f4dfa6a1e19f10badf596e800983ce2
mridwan413 <mridwantkj4@gmail.com>
2019-02-25 19:04:55 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+Route::get('/produk', function () {
+    return view('produk');
+})->name('produk');
+Route::get('/detail-produk/{id}', function ($id) {
+    return view('detail-produk',array('id'=>$id));
+})->name('detail-produk');
+Route::get('/keranjang', function () {
+    return view('keranjang');
+})->name('keranjang');
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
